4035c5fd575bf569fa79ba3daefbd10788a95579
JulezD3v <jul3zdev@gmail.com>
2026-01-08 16:56:18 +0300

    Added provider at Material app

--- lib/main.dart
+++ lib/main.dart
@@ -3,34 +3,39 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:eight_all_luck/widget/ball_area_widget.dart';
 import 'package:eight_all_luck/bloc/eight_ball_luck_bloc.dart';
-import 'package:eight_all_luck/widget/shake_anim_widget.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
 void main() {
-  runApp(const MaterialApp(
-    home: Magic8BallPage(), debugShowCheckedModeBanner:false),
-    );
-    
-} 
+  runApp(
+     MaterialApp(
+      home: BlocProvider( //for this Error: Could not find the correct Provider<EightBallBloc> above this Magic8BallPage Widget
+        create: (_) => EightBallBloc(),
+        child: Magic8BallPage(),
+      ),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
 
 class Magic8BallPage extends StatelessWidget {
   const Magic8BallPage({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      
       create: (context) => EightBallBloc(),
       child: Center(
         //mainAxisAlignment: MainAxisAlignment.center
         child: Scaffold(
           backgroundColor: Colors.white,
-        
-        
+
           appBar: AppBar(
-            title:  Text('Magic 8 Ball'),
-            titleTextStyle: GoogleFonts.lato(fontSize: 20, fontWeight: FontWeight.w600, color: Colors.black),
+            title: Text('Magic 8 Ball'),
+            titleTextStyle: GoogleFonts.lato(
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+              color: Colors.black,
+            ),
             backgroundColor: Colors.white,
             //foregroundColor: Colors.black,
             elevation: 0,
@@ -41,55 +46,61 @@ class Magic8BallPage extends StatelessWidget {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-        
-                  BallArea(),// the ball
-                  SizedBox(height: 30,),
-        
-        //Users input
+                  BallArea(), // the ball
+                  SizedBox(height: 30),
+
+                  //Users input
                   SizedBox(
-                  width: 300,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: "Input your text here",
-                      hintStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w300),
-                      border: OutlineInputBorder(
-                       borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                       
-                    )),
+                    width: 300,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Input your text here",
+                        hintStyle: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w300,
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-                SizedBox(height: 20,),
-        
-        //Button
-                OutlinedButton(onPressed: (){
-                  //Shake(child: BallArea(), trigger:true ); ==> widget is being created 
-                  //context.read<EightBallBloc>().add(BallArea()); tomorrows problem
-                },
-                style: OutlinedButton.styleFrom(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 40,
-                    vertical:14,
+                  SizedBox(height: 20),
+
+                  //Button
+                  GestureDetector(
+                    //tomorrows problem still not working,
+                    child: OutlinedButton(
+                      onPressed: () {
+                        context.read<EightBallBloc>().add(ShakeBall());
+                      },
+                      style: OutlinedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 40,
+                          vertical: 14,
+                        ),
+                        side: BorderSide(color: Colors.black),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+
+                      child: Text(
+                        "SHAKE AGAIN",
+                        style: GoogleFonts.lato(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w300,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
                   ),
-                  side: BorderSide(color: Colors.black),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30)
-                  )
-                ), 
-                
-                child:  Text(
-                  "SHAKE AGAIN",
-                  style: GoogleFonts.lato(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w300,
-                    color: Colors.black
-                  ),
-                ))
                 ],
               ),
-            )),
+            ),
+          ),
         ),
       ),
     );
   }
 }
-
